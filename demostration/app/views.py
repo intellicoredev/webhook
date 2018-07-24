fe01d48b977399aaa67ea096e97e02828c828eb4
FT <ft@email.com>
2018-07-24 14:25:33 -0500

    "Version 1.0.4"

--- demostration/app/views.py
+++ demostration/app/views.py
@@ -25,6 +25,47 @@ from time import strptime, strftime, mktime, gmtime
 
 from sigfoxapi import Sigfox
 
+import inspect
+import logging
+
+
+def function_logger(file_level, console_level=None):
+    function_name = inspect.stack()[1][3]
+    logger = logging.getLogger(function_name)
+    logger.setLevel(logging.DEBUG)  # By default, logs all messages
+
+    if console_level != None:
+        ch = logging.StreamHandler()  # StreamHandler logs to console
+        ch.setLevel(console_level)
+        ch_format = logging.Formatter('%(asctime)s - %(message)s')
+        ch.setFormatter(ch_format)
+        logger.addHandler(ch)
+
+    fh = logging.FileHandler("{0}.log".format(function_name))
+    fh.setLevel(file_level)
+    fh_format = logging.Formatter('%(asctime)s - %(lineno)d - %(levelname)-8s - %(message)s')
+    fh.setFormatter(fh_format)
+    logger.addHandler(fh)
+
+    return logger
+
+
+def f1():
+    f1_logger = function_logger(logging.DEBUG, logging.ERROR)
+    f1_logger.debug('debug message')
+    f1_logger.info('info message')
+    f1_logger.warn('warn message')
+    f1_logger.error('error message')
+    f1_logger.critical('critical message')
+
+
+def f2():
+    f2_logger = function_logger(logging.WARNING)
+    f2_logger.debug('debug message')
+    f2_logger.info('info message')
+    f2_logger.warn('warn message')
+    f2_logger.error('error message')
+    f2_logger.critical('critical message')
 
 
 def datetime_converter(datetime_string):
@@ -506,16 +547,27 @@ def sigfox_webhook(request):
     # Sometimes the payload comes in as the request body, sometimes it comes in
     # as a POST parameter. This will handle either case.
 
-    pr_print = pretty_request(request)
-    print("DEBUG:Method: {}".format(pr_print))
+    #f1_logger = function_logger(logging.DEBUG, logging.ERROR)
 
-    print("DEBUG:Request print {}".format(pr_print))
+    f2_logger = function_logger(logging.WARNING)
+    f2_logger.debug('debug message {}'.format(request.body))
+
+
+    logging.shutdown()
+
+    pr_print = pretty_request(request)
+    print("DEBUG:sigfox_webhook:pr_print: {}".format(pr_print))
 
     if 'data' in request.POST:
         data = json.loads(request.POST['data'])
         print(('sigfox_webhook Log: 1. POST <data> received the {} device'.format(data)))
     else:
-        data = json.loads(request.body)
+        body_unicode = request.body.decode('utf-8')
+        body_data = json.loads(body_unicode)
+        print("DEBUG:sigfox_webhook:body_data: {}".format(body_data))
+
+        data = body_data
+        #data = json.loads(request.body)
         print("DEBUG:Body: {}".format(request.body))
         #print("{}".format(request.headers))
         #print(('sigfox_webhook Log: 2. <body> received the {} device'.format(data)))
